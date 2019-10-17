e75d631fda9523e275af137a1b574d93ce5bc8f4
glannath <jakub.mateja@outlook.com>
2019-10-17 22:02:59 +0200

    seting up and testing connection to database

--- web-customer-manager/src/com/glannath/testDB/TestDbServlet.java
+++ web-customer-manager/src/com/glannath/testDB/TestDbServlet.java
@@ -1,12 +1,14 @@
 package com.glannath.testDB;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import java.sql.*;
 /**
  * Servlet implementation class TestDbServlet
  */
@@ -14,19 +16,45 @@ import javax.servlet.http.HttpServletResponse;
 public class TestDbServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public TestDbServlet() {
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+	
+	
+	//setup connection variables
+	
+	String user = "customeruser";
+	String password = "customeruser";
+	
+	String jdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false&serverTimezone=UTC";
+	String driver = "com.mysql.cj.jdbc.Driver";
+			
+
+	
+	//get connection to database
+	
+	try {
+		PrintWriter out = response.getWriter();
+		
+		out.println("Connecting to database: " + jdbcUrl);
+		
+		Class.forName(driver);
+		
+		Connection myConnection = DriverManager.getConnection(jdbcUrl, user, password);
+		
+		out.println("Connection sucesfull!");
+				
+		
+	}
+	catch(Exception ex) {
+		ex.printStackTrace();
+		throw new ServletException(ex);
 	}
 
 }
+}
+
+
