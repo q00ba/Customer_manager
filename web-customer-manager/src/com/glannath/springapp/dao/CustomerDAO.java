8cc255e10968e0576b2fa91888459173a160f170
glannath <jakub.mateja@outlook.com>
2019-10-22 09:17:52 +0200

    Creating customer DAO

---
+++ web-customer-manager/src/com/glannath/springapp/dao/CustomerDAO.java
@@ -0,0 +1,10 @@
+package com.glannath.springapp.dao;
+
+import java.util.List;
+
+import com.glannath.springapp.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+}
