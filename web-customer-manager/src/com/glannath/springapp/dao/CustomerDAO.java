45fb66762fe126ef83e21a8ccccab88a18cdaf0d
glannath <jakub.mateja@outlook.com>
2019-10-27 17:25:21 +0100

    connecting with DB

--- web-customer-manager/src/com/glannath/springapp/dao/CustomerDAO.java
+++ web-customer-manager/src/com/glannath/springapp/dao/CustomerDAO.java
@@ -7,4 +7,6 @@ import com.glannath.springapp.entity.Customer;
 public interface CustomerDAO {
 
 	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
 }
--- web-customer-manager/src/com/glannath/springapp/service/CustomerService.java
+++ web-customer-manager/src/com/glannath/springapp/service/CustomerService.java
@@ -7,4 +7,6 @@ import com.glannath.springapp.entity.Customer;
 public interface CustomerService {
 
 	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
 }
