68bd4470b6731c7bb8f924700c3ac201e6459494
glannath <jakub.mateja@outlook.com>
2019-11-03 18:37:24 +0100

    adding @transactional anotation in Service

--- web-customer-manager/src/com/glannath/springapp/service/CustomerServiceImpl.java
+++ web-customer-manager/src/com/glannath/springapp/service/CustomerServiceImpl.java
@@ -45,6 +45,7 @@ public class CustomerServiceImpl implements CustomerService {
 	}
 
 	@Override
+	@Transactional
 	public List<Customer> searchCustomers(String theSearchName) {
 		
 		return customerDAO.searchCustomers(theSearchName);
