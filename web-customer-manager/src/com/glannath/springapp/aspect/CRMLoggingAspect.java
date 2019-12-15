de81bfa7648add0c717a2450e163b21b276b6d85
glannath <jakub.mateja@outlook.com>
2019-12-15 18:44:59 +0100

    adding pointcuts and @Before, @AfterReturning aspects

--- web-customer-manager/src/com/glannath/springapp/aspect/CRMLoggingAspect.java
+++ web-customer-manager/src/com/glannath/springapp/aspect/CRMLoggingAspect.java
@@ -1,6 +1,11 @@
 package com.glannath.springapp.aspect;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+
 import java.util.logging.Logger;
 import org.springframework.stereotype.Component;
 
@@ -10,12 +15,48 @@ public class CRMLoggingAspect {
 
 	//setup logger
 	
-	private Logger myLoger = Logger.getLogger(getClass().getName());
+	private Logger myLogger = Logger.getLogger(getClass().getName());
 	
 	// setup pointcut declarations
 	
+	@Pointcut("execution(* com.glannath.springapp.controller.*.*(..))")
+	private void forControllerPackage() {}
+	
+	@Pointcut("execution(* com.glannath.springapp.service.*.*(..))")
+	private void forServicePackage() {}
+	
+	@Pointcut("execution(* com.glannath.springapp.dao.*.*(..))")
+	private void forDAOPackage() {}
+	
+	@Pointcut("forControllerPackage() || forServicePackage() || forDAOPackage()")
+	private void forAppFlow() {}
+	
 	// add @Before advice
 	
+	@Before("forAppFlow()")
+	public void before(JoinPoint theJoinPoint) {
+		
+		String theMethod = theJoinPoint.getSignature().toShortString();
+		myLogger.info("====>> in @Before: calling method: "+ theMethod);
+		
+		Object[] args = theJoinPoint.getArgs();
+		
+		for (Object tempArg : args) {
+			myLogger.info("======>> argument: "+ tempArg);
+		}
+		
+	}
+	
 	// add @AfterReturinig advice
+	
+	@AfterReturning(pointcut="forAppFlow()", returning="theResult")
+	public void afterReturning(JoinPoint theJoinPoint, Object theResult) {
+		
+		String theMethod = theJoinPoint.getSignature().toShortString();
+		myLogger.info("====>> in @AfterReturning: calling method: "+ theMethod);
+		
+		myLogger.info("=====>> result: "+ theResult);
+		
+	}
 	
 }
